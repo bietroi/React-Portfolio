bf5029d7caf1efcbc63d65df2a399dd530980334
bietroi <143153190+bietroi@users.noreply.github.com>
2023-12-19 11:46:21 +0700

    My portfolio

---
+++ src/components/About.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
+            <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full ">
+                <div className="pb-8">
+                    <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
+                </div>
+                <p className="text-xl mt-20">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut atque neque deleniti quae culpa accusamus tempora nisi eligendi odio quaerat eos, reiciendis saepe iusto officiis voluptatum perspiciatis dicta pariatur. Corporis quos dignissimos recusandae quia porro laboriosam nemo eos libero, rerum voluptatum exercitationem esse ducimus totam sit amet deleniti harum maiores.
+                </p>
+                <br />
+                <p className="text-xl">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione nisi aliquid delectus sequi deleniti laboriosam iusto dolorem culpa recusandae accusamus minus sit suscipit, eius id corporis facilis quia voluptatum illum ut repellat tempora inventore autem adipisci dolorum. Omnis, nulla qui. Praesentium quisquam minus placeat adipisci odio, blanditiis illum delectus nesciunt?
+                </p>
+
+
+            </div>
+        </div>
+    );
+};
+
+export default About;
